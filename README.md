e0a197b8b6b3cecbf448cc2a1f00d3b5f27708d1
Ralphkaram10 <Ralphkaram10@gmail.com>
2024-09-08 23:17:48 +0200

    Add README.md and use pyproject.toml

---
+++ README.md
@@ -0,0 +1,10 @@
+#  Image\_classification_deep_learning
+
+This repository is used to train a deep neural network image classifier and test it on a specific image. 
+
+>**How to Use**:
+>  * Install the necessary python dependencies (found in pyproject.toml) with "pip install -e ." preferably in a venv or conda environment.  
+>  * The needed configuration files needed to for this repository are located at src/config. Update them when needed.
+>  * Use train.py to train an image classifier.
+>  * Use predict.py to classify a specific image.
+
--- src/predict.py
+++ src/predict.py
@@ -1,7 +1,7 @@
 """Module to classify a specified image"""
 import torch
 from PIL import Image
-from models.model import resnet18
+from models.model import resnet18, ResNet
 from common.utils import load_yaml_config
 from common import datakeywords as dk
 from DL.classification_dl import preprocess_image
@@ -10,7 +10,7 @@ cfg_train = load_yaml_config("src/config/config_train.yaml")
 cfg_predict = load_yaml_config("src/config/config_predict.yaml")
 
 
-def model_load():
+def model_load()->ResNet:
     """Load the trained model based on train and predict configs"""
     net = resnet18(num_classes=cfg_train[dk.NUM_CLASSES_KEY])
     net.load_state_dict(
@@ -19,20 +19,20 @@ def model_load():
     return net
 
 
-def load_preprocess_image():
+def load_preprocess_image()->torch.Tensor:
     """Load and preprocess the image based on predict config"""
     im = Image.open(cfg_predict[dk.IMAGE_PATH_KEY]).convert("RGB")
     return preprocess_image(im,to_predict=True)
 
 
-def perform_inference(im_tensor, model):
+def perform_inference(im_tensor: torch.Tensor, model: ResNet)->int:
     """Perform inference on an image tensor using the specified model"""
     # Perform inference
     with torch.no_grad():
         output = model(im_tensor)
 
     # Get the predicted class index
-    predicted_class_idx = torch.argmax(output).item()
+    predicted_class_idx = int(torch.argmax(output).item())
     return predicted_class_idx
 
 
